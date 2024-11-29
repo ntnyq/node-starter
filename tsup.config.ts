0bd30d5c6d4b89c3d8ba576bf1ce91489fffbb90
ntnyq <ntnyq13@gmail.com>
2024-11-29 23:06:35 +0800

    chore: update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  dts: true,
-  clean: true,
   cjsInterop: true,
+  clean: true,
+  dts: true,
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   target: ['es2022', 'node18'],
 })

5f20989dbcfd81e652a4bcc18e581e1634df97ac
ntnyq <ntnyq13@gmail.com>
2025-05-27 17:18:58 +0800

    chore: remove engine check

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,7 +1,4 @@
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-export const __dirname: string = fileURLToPath(new URL('.', import.meta.url))
 
 export const resolve = (...args: string[]): string =>
-  path.resolve(__dirname, '..', ...args)
+  path.resolve(import.meta.dirname, '..', ...args)
